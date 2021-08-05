588bdbfdaa4d7f365a72074e5ffd3267dc9c5541
mervenrkrky <burcu90132@gmail.com>
2021-08-05 21:48:56 +0300

    Add project files.

---
+++ ClassMetodDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetodDemo
+{
+    class MusteriManager
+    {
+        public void musteriEkle(Musteri musteri)
+        {
+            Console.WriteLine( musteri.FirstName +" adlı müşteri eklendi");
+        }
+
+        public void musteriListele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.LastName+ " soyadlı muşteri listelendi");
+        }
+
+        public void musteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.FirstName + " adlı musteri silindi");
+        }
+    }
+}
---
+++ ClassMetodDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassMetodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.FirstName = "Mervenur";
+            musteri.LastName = "Karakaya";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriEkle(musteri);
+            musteriManager.musteriListele(musteri);
+            musteriManager.musteriSil(musteri);
+        }
+    }
+}
